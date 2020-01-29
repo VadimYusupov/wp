2191141b38d9d015e27aad1aa792476cb5c8766e
VadimYusupov <vusupov@idscan.net>
2020-01-29 13:29:17 +0500

    webpack. part2

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+    entry: {
+        app: './src/index.js',
+        another: './src/another-module.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
     },
     module: {
         rules: [
             {
-                test: /\./css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ],
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Caching',
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     }
-}
+};
